3af52e6512e7a9ce27522a938d8aad7122e02e4e
zxbsmk <56811692+ZhengJun-AI@users.noreply.github.com>
2020-08-23 18:28:56 +0800

    Update Train Function and Change Batchsize
    Speed up the training process

--- train.py
+++ train.py
@@ -10,15 +10,17 @@ from torch.autograd import Variable
 from torch.nn import functional as F
 from torch import nn
 import torch
-from random import randint
+from random import randint, sample
 from PIL import Image
 import numpy as np
 import struct
 import gzip
 import os
 
+# from tensorboardX import SummaryWriter
 
-torch.cuda.set_device(4)
+
+torch.cuda.set_device(5)
 
 
 class MoireCNN(nn.Module):
@@ -125,16 +127,18 @@ class MoireCNN(nn.Module):
 
         x=x1+x2+x3+x4+x5
         
-        return torch.clamp(x, 0, 1)
+        return x
 
 
-def train(epoch):
+def train(epoch, lr):
     model.train()
+
     for batch_idx, (data, target) in enumerate(train_loader):
         if use_gpu:
-            data, target = data.cuda(), target.cuda()
+            data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
         data, target = Variable(data), Variable(target)
 
+        optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=0.00001)
         optimizer.zero_grad()
         output = model(data)
         loss = criterion(output, target)
@@ -146,15 +150,55 @@ def train(epoch):
                 100. * batch_idx / len(train_loader), loss.data))
 
 
+def test(epoch):
+    model.eval()
+
+    idx = 0
+    loss_sum = 0.0
+    for (data, target) in test_loader:
+        if use_gpu:
+            data, target = data.cuda(non_blocking=True), target.cuda(non_blocking=True)
+        data, target = Variable(data), Variable(target)
+
+        with torch.no_grad():
+            output = model(data)
+            loss = criterion(output, target)
+
+        loss_sum += loss.data
+        idx += 1
+    loss_sum /= idx
+
+    print('Test Epoch: {} \tLoss: {:.6f}'.format(
+                epoch, loss_sum))
+
+    global pre_loss, lr, best_loss
+    if loss_sum > pre_loss:
+        lr *= 0.9
+    
+    if loss_sum < best_loss:
+        best_loss = loss_sum
+        torch.save(model, "moire-1.pth")
+
+    pre_loss = loss_sum
+
+
 class MoirePic(data.Dataset):
-    def __init__(self, rootX, rootY, train=True):
-        self.train=train
+    def __init__(self, rootX, rootY, training=True):
         self.picX=[rootX+i for i in os.listdir(rootX)]
         self.picY=[rootY+i for i in os.listdir(rootY)]
         self.picX.sort()
         self.picY.sort()
-        self.picX=self.picX
-        self.picY=self.picY
+        # self.picX=self.picX[:40]
+        # self.picY=self.picY[:40]
+        self.Len=len(self.picX)
+
+        if not training:
+            L = sample(range(self.Len), self.Len//10)
+            tempX = [self.picX[i] for i in L]
+            tempY = [self.picY[i] for i in L]
+            self.picX=tempX
+            self.picY=tempY
+            self.Len=len(L)
         
     def __getitem__(self, index):
         tf=transforms.ToTensor()
@@ -170,39 +214,51 @@ class MoirePic(data.Dataset):
             return (data[:,width1:width2,height1:height2],
             label[:,width1:width2,height1:height2])
 
-        if self.train:
-            pathX, pathY=self.picX[index], self.picY[index]
-            imgX, imgY=Image.open(pathX), Image.open(pathY)
-            return rand_crop(tf(imgX), tf(imgY))
+        pathX, pathY=self.picX[index], self.picY[index]
+        imgX, imgY=Image.open(pathX), Image.open(pathY)
+        return rand_crop(tf(imgX), tf(imgY))
     
     def __len__(self):
-        return len(self.picX)
+        return self.Len
+
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        m.weight.data.normal_(mean=0.0, std=0.01)
+        m.bias.data.fill_(0)
 
 
 dataset = MoirePic("/data_new/moire/trainData/source/",
                    "/data_new/moire/trainData/target/")
-# dataset=MoirePic("/home/zhengjun/moire/moire-data/ValidationMoire/",
-#                 "/home/zhengjun/moire/moire-data/ValidationClear/")
+testdataset = MoirePic("/data_new/moire/trainData/source/",
+                   "/data_new/moire/trainData/target/", False)
 use_gpu = torch.cuda.is_available()
-batch_size = 32
+batch_size = 8
 kwargs = {'num_workers': 14, 'pin_memory': True}
 train_loader = DataLoader(dataset=dataset, shuffle=True,
                           batch_size=batch_size, **kwargs)
+test_loader = DataLoader(dataset=testdataset, shuffle=True,
+                          batch_size=batch_size, **kwargs)  
 
-model = MoireCNN()
+# model = MoireCNN()
+model = torch.load("moire-1.pth")
+# model.apply(weights_init)
+
+# with SummaryWriter(comment='MoireCNN') as w:
+#     w.add_graph(model, (x, ))
 
 if use_gpu:
     model = model.cuda()
-    # model = nn.DataParallel(model, device_ids=[3, 4, 5, 6])
+    # model = nn.DataParallel(model)
     print('USE GPU')
 else:
     print('USE CPU')
 
 criterion = nn.MSELoss()
-lr = 0.0001
-optimizer = torch.optim.Adam(model.parameters(), lr=lr, betas=(0.9, 0.99), weight_decay=0.00001)
+lr = 0.00004
+pre_loss = 100.0
+best_loss = 100.0
 
-for epoch in range(101):
-    train(epoch)
-    if epoch % 10 == 0:
-        torch.save(model, "moire-1.pth")
+for epoch in range(100):
+    train(epoch, lr)
+    test(epoch)
